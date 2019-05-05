0ddae1b2cc945b63b209c0ef01bc3ba9beb54267
Jack Frost <albarrankevin97@gmail.com>
2019-05-04 23:52:11 -0700

    another change

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,22 @@ require("dotenv").config();
 if (process.env.CLEARDB_DATABASE_URL) {
     connection = mysql.createConnection({
         host: "us-cdbr-iron-east-02.cleardb.net",
+        port:3306,
         user: "b1718fad4e37c6",
         password: "cc47b9b9",
         database: "heroku_b1718fad4e37c6"
     });
 } 
-else{
+// else{
     
-    connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: "burgers_db"
-    });
-}
+//     connection = mysql.createConnection({
+//         host: process.env.DB_HOST,
+//         port: 3306,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: "burgers_db"
+//     });
+// }
 
 connection.connect(function(err) {
     if (err) {

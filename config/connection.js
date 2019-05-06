29c0d4683212050748c14500bb3c0a9ab9f039f3
Jack Frost <albarrankevin97@gmail.com>
2019-05-06 15:19:45 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
 
-    
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+}
 
 connection.connect(function(err) {
     if (err) {
